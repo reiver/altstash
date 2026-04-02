4d1853b11db90f89f71a572dba8d91499c4171fa
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-04-01 19:45:20 -0700

    libconfig

--- lib/config/config.go
+++ lib/config/config.go
@@ -14,10 +14,17 @@ type Config struct {
 	DataDirectory string `json:"data_directory"`
 }
 
-// Load reads the config from configDir/config.json.
+// LoadConfigDir reads the config from configDir/config.json and returns a [Config].
+// The config data is expected to be JSON.
+//
+//
+// If the config data does not contain a "data_directory" field, or the field
+// has an empty value, then the value is set to `defaultDataDir`.
+//
 // If the file doesn't exist, returns a Config with defaultDataDir.
-// The caller provides the paths so that lib/ does not import cfg/.
-func Load(configDir string, defaultDataDir string) (Config, error) {
+//
+// See also: [LoadFromBytes].
+func LoadConfigDir(configDir string, defaultDataDir string) (Config, error) {
 	configPath := filepath.Join(configDir, "config.json")
 
 	data, err := os.ReadFile(configPath)
@@ -30,9 +37,20 @@ func Load(configDir string, defaultDataDir string) (Config, error) {
 		return Config{}, erorr.Wrap(err, "could not read config file")
 	}
 
+	return LoadFromBytes(data, defaultDataDir)
+}
+
+// Load reads the config from a []byte and returns a [Config].
+// The config data is expected to be JSON.
+//
+// If the config data does not contain a "data_directory" field, or the field
+// has an empty value, then the value is set to `defaultDataDir`.
+//
+// See also: [LoadConfigDir].
+func LoadFromBytes(bytes []byte, defaultDataDir string) (Config, error) {
 	var config Config
 
-	err = json.Unmarshal(data, &config)
+	err := json.Unmarshal(bytes, &config)
 	if nil != err {
 		return Config{}, erorr.Wrap(err, "could not parse config JSON")
 	}
